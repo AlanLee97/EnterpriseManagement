fa7849c4625cf10e363a862b4e402868622695f0
李步官 <42601044+AlanLee97@users.noreply.github.com>
2019-10-22 09:46:11 +0800

    2019-10-22 9:40 更新内容： 1、整合Thymeleaf 2、添加首页页面 3、加入添加用户页面 4、美化界面

--- src/main/java/com/zdxh/controller/IndexController.java
+++ src/main/java/com/zdxh/controller/IndexController.java
@@ -2,12 +2,14 @@ package com.zdxh.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hello Spring Boot!";
+    @RequestMapping("/")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("/index");
+        return mv;
     }
 }
--- src/main/java/com/zdxh/controller/UserController.java
+++ src/main/java/com/zdxh/controller/UserController.java
@@ -5,7 +5,9 @@ import com.zdxh.entity.User;
 import com.zdxh.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -13,6 +15,17 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserService userService;
+
+    /**
+     * 页面：添加用户
+     * @return
+     */
+    @RequestMapping("/viewAddUser")
+    public ModelAndView viewAddUser(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/viewAddUser");
+        return mv;
+    }
 
     /**
      * 增：添加用户
