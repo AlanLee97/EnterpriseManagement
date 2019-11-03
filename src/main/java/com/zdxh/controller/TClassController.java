dc13f3b098443fffa205d22cb1679e934e9570f2
李步官 <42601044+AlanLee97@users.noreply.github.com>
2019-11-03 13:24:33 +0800

    2019-11-3 13:25 更新内容： 1、更新数据库文件 2、添加商品功能（未完善）

--- src/main/java/com/zdxh/controller/TClassController.java
+++ src/main/java/com/zdxh/controller/TClassController.java
@@ -1,7 +1,30 @@
 package com.zdxh.controller;
 
+import com.zdxh.entity.Api;
+import com.zdxh.entity.TClass;
+import com.zdxh.service.TClassService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TClassController {
+
+    @Autowired
+    TClassService classService;
+
+    @RequestMapping("/addClassDoApi")
+    @ResponseBody
+    public Api addClass(String classNum, String className){
+        TClass tClass = new TClass();
+        tClass.setClassNum(classNum);
+        tClass.setClassName(className);
+        int i = classService.addClass(tClass);
+        if (i == 1){
+            return new Api(200, "ok", tClass);
+        }else {
+            return new Api(400, "error", null);
+        }
+    }
 }
--- src/main/java/com/zdxh/controller/TProductController.java
+++ src/main/java/com/zdxh/controller/TProductController.java
@@ -1,7 +1,47 @@
 package com.zdxh.controller;
 
+import com.zdxh.entity.Api;
+import com.zdxh.entity.TProduct;
+import com.zdxh.service.TProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 @Controller
 public class TProductController {
+    @Autowired
+    TProductService productService;
+
+    @RequestMapping("/addProductDoApi")
+    @ResponseBody
+    public Api addProduct(String productName,
+                          Double productPrice,
+
+                          Integer productNumber,
+                          String productIntroduce
+//                          Date productDate
+    ){
+
+        TProduct product = new TProduct();
+
+        product.setProductName(productName);
+        product.setProductPrice(productPrice);
+//        product.setProductImg(productimg);
+        product.setProductNumber(productNumber);
+        product.setProductIntroduce(productIntroduce);
+//        product.setProductDate(productDate);
+
+
+        int i = productService.addProduct(product);
+
+        if (i == 1){
+            return new Api(200, "ok", product);
+        }else {
+            return new Api(400, "error", null);
+        }
+
+    }
 }
